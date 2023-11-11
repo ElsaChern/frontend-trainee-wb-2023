4b40cb0e316edd35542638de6f84cc99e3d2e43d
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2023-11-11 18:11:15 +0300

    calculate product price

--- js/hideCards.js
+++ js/hideCards.js
@@ -3,19 +3,25 @@ const hideCardsInStockBtn = document.querySelector("#hide_cards_1");
 const hiddenCardsInStock = document.querySelector("#hidden_goods_in_stock");
 const selectAll = document.querySelector("#select_all");
 const totalGoodsPrice = document.querySelector("#total_goods_price");
+
 let isBtnInStockClicked = true;
 
 hideCardsInStockBtn.addEventListener("click", () => {
+  const totalProductAmount =
+    document.querySelector("#total_amount").textContent;
+  const totalCurrentSum = document.querySelector("#total_sum").textContent;
+
   if (isBtnInStockClicked) {
     hiddenCardsInStock.style.display = "none";
     hideCardsInStockBtn.style.transform = "rotate(180deg)";
     selectAll.style.display = "none";
-    totalGoodsPrice.textContent = "266 товаров · 2 100 569 сом";
+    totalGoodsPrice.textContent = `${totalProductAmount} · ${totalCurrentSum} сом`;
     totalGoodsPrice.classList.add("show");
   } else {
     hiddenCardsInStock.style.display = "flex";
     hideCardsInStockBtn.style.transform = "rotate(0deg)";
     selectAll.style.display = "flex";
+    totalGoodsPrice.textContent = "";
     totalGoodsPrice.classList.remove("show");
   }
   isBtnInStockClicked = !isBtnInStockClicked;
--- js/products.js
+++ js/products.js
@@ -6,31 +6,44 @@ const products = productsInCartContainer.querySelectorAll(".product");
 let totalCurrentSum = document.querySelector("#total_sum");
 let totalOldtSum = document.querySelector("#total_old_sum");
 let totalDiscount = document.querySelector("#total_discount");
-const checkboxAllGoods = document.querySelector("#checked_all"); // Сделать проверку на кнопку
+let totalProductAmount = document.querySelector("#total_amount");
 
-const calculateTotal = () => {
+const showCartNumber = () => {
+  let counter = document.querySelector(".quantity-basket");
+  counter.textContent = products.length;
+};
+
+showCartNumber();
+
+export const calculateTotal = () => {
   let totalCurrentSumValue = 0;
   let totalOldSumValue = 0;
   let totalDiscountValue = 0;
+  let totalAmount = 0;
 
   const checkedProducts = [...products].filter((product) => {
     return product.querySelector(".checkbox").checked;
   });
 
   checkedProducts.forEach((product) => {
-    let productCurrentPrice = product
+    let priductCurrentAmount = +product.querySelector('input[name="counter"]')
+      .value;
+    let productCurrentPrice = +product
       .querySelector("#current_price")
-      .textContent.replaceAll(" ", "");
-    let productOldPrice = product
+      .textContent.replace(/\D/g, "");
+    let productOldPrice = +product
       .querySelector("#old_price")
-      .textContent.replaceAll(" ", "");
-    let prodictDiscount = Number(productOldPrice) - Number(productCurrentPrice);
+      .textContent.replace(/\D/g, "");
+    let productDiscount = productOldPrice - productCurrentPrice;
 
-    totalCurrentSumValue += +productCurrentPrice;
-    totalOldSumValue += +productOldPrice;
-    totalDiscountValue += prodictDiscount;
+    totalAmount += priductCurrentAmount;
+    totalCurrentSumValue += productCurrentPrice;
+    totalOldSumValue += productOldPrice;
+    totalDiscountValue += productDiscount;
   });
 
+  totalProductAmount.textContent =
+    parseInt(String(totalAmount)).toLocaleString("ru") + " товара";
   totalCurrentSum.textContent = parseInt(
     String(totalCurrentSumValue),
   ).toLocaleString("ru");
@@ -42,27 +55,76 @@ const calculateTotal = () => {
   ).toLocaleString("ru");
 };
 
-// const calculatePrice = () => {};
+// Функция, которая отображает цену товара для каждого из продуктов
 
 products.forEach((product) => {
   const checkbox = product.querySelector(".checkbox");
-  // const counterMinus = product.querySelector(".minus");
-  // const counterPlus = product.querySelector(".plus");
-  // const counterInput = product.querySelector(".value");
-  // console.log(checkbox);
+  const counterMinus = product.querySelector(".minus");
+  const counterPlus = product.querySelector(".plus");
+  const counterInput = product.querySelector('input[name="counter"]');
+  const productCurrentPrices = product.querySelectorAll("#current_price");
+  const productOldPrices = product.querySelectorAll("#old_price");
+  const itemsLeft = +product
+    .querySelector(".remainder")
+    ?.textContent.replace(/\D/g, "");
+
+  const checkCounterBtn = (value) => {
+    if (value <= 1) {
+      counterMinus.disabled = true;
+    } else {
+      counterMinus.disabled = false;
+    }
+    if (value >= itemsLeft) {
+      counterPlus.disabled = true;
+    } else {
+      counterPlus.disabled = false;
+    }
+  };
+
+  const calculatePrice = (newCount) => {
+    let pricePerItem = +product.dataset.price;
+    checkCounterBtn(newCount);
+    counterInput.value = newCount;
+
+    const productCurrentPriceValue = Math.ceil(pricePerItem * newCount * 0.35);
+    productCurrentPrices.forEach((item) => {
+      item.textContent = parseInt(
+        String(productCurrentPriceValue),
+      ).toLocaleString("ru");
+    });
+    const productOldPriceValue = pricePerItem * newCount;
+    productOldPrices.forEach((item) => {
+      item.textContent = parseInt(String(productOldPriceValue)).toLocaleString(
+        "ru",
+      );
+    });
+    calculateTotal();
+  };
+
+  checkCounterBtn(+counterInput.value);
 
   checkbox.addEventListener("change", () => {
     calculateTotal();
   });
 
-  // counterMinus.addEventListener("click", calculatePrice);
-  // counterPlus.addEventListener("click", calculatePrice);
-  // counterInput.addEventListener("click", calculatePrice);
+  counterMinus.addEventListener("click", () => {
+    calculatePrice(+counterInput.value - 1);
+  });
+
+  counterPlus.addEventListener("click", () => {
+    calculatePrice(+counterInput.value + 1);
+  });
+
+  counterInput.addEventListener("change", (e) => {
+    const newValue = +e.target.value;
+    const oldValue = +e.target.defaultValue;
+
+    if (newValue > itemsLeft || newValue < 1) {
+      calculatePrice(oldValue);
+    } else {
+      calculatePrice(newValue);
+    }
+  });
 });
 
 // Функция, которая отображает количество товаров в корзине при загрузке страницы.
-
-// const initCartNumber = () => {
-//   let counter = document.querySelector(".quantity-basket");
-//   counter.textContent = products.length;
-// };
