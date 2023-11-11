d07403ae8568d2693439a90daee057099c54d28a
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2023-11-11 21:32:16 +0300

    refactor tooltips and some styles

--- js/tooltips.js
+++ js/tooltips.js
@@ -1,8 +1,3 @@
-const tooltipGreenButton = document.querySelector("#text_green_bnt");
-const tooltipInfo = document.querySelector("#tooltip_info");
-const tooltipGreenButton1 = document.querySelector("#text_green_bnt_1");
-const tooltipInfo1 = document.querySelector("#tooltip_info_1");
-
 const toggleTooltip = (tipButton, infoBlock) => {
   tipButton.addEventListener("mouseover", () => {
     infoBlock.style.display = "flex";
@@ -13,27 +8,25 @@ const toggleTooltip = (tipButton, infoBlock) => {
   });
 };
 
-toggleTooltip(tooltipGreenButton, tooltipInfo);
-toggleTooltip(tooltipGreenButton1, tooltipInfo1);
+for (let i = 1; i < 3; i++) {
+  let tooltipGreenButton = document.querySelector(`#text_green_bnt_${i}`);
+  let tooltipInfo = document.querySelector(`#tooltip_info_${i}`);
 
-const tooltipProdBtn1 = document.querySelector("#tooltip_button_prod_1");
-const tooltipProdInfo1 = document.querySelector("#tooltip_info_prod_1");
-const tooltipProdBtn2 = document.querySelector("#tooltip_button_prod_2");
-const tooltipProdInfo2 = document.querySelector("#tooltip_info_prod_2");
-const tooltipProdBtn3 = document.querySelector("#tooltip_button_prod_3");
-const tooltipProdInfo3 = document.querySelector("#tooltip_info_prod_3");
+  toggleTooltip(tooltipGreenButton, tooltipInfo);
+}
 
-toggleTooltip(tooltipProdBtn1, tooltipProdInfo1);
-toggleTooltip(tooltipProdBtn2, tooltipProdInfo2);
-toggleTooltip(tooltipProdBtn3, tooltipProdInfo3);
+for (let i = 1; i < 4; i++) {
+  let tooltipProdBtn = document.querySelector(`#tooltip_button_prod_${i}`);
+  let tooltipProdInfo = document.querySelector(`#tooltip_info_prod_${i}`);
 
-const tipDiscountBtn1 = document.querySelector("#tooltip_button_discount_1");
-const tipDiscount1 = document.querySelector("#tooltip_discount_1");
-const tipDiscountBtn2 = document.querySelector("#tooltip_button_discount_2");
-const tipDiscount2 = document.querySelector("#tooltip_discount_2");
-const tipDiscountBtn3 = document.querySelector("#tooltip_button_discount_3");
-const tipDiscount3 = document.querySelector("#tooltip_discount_3");
+  toggleTooltip(tooltipProdBtn, tooltipProdInfo);
+}
 
-toggleTooltip(tipDiscountBtn1, tipDiscount1);
-toggleTooltip(tipDiscountBtn2, tipDiscount2);
-toggleTooltip(tipDiscountBtn3, tipDiscount3);
+for (let i = 1; i < 4; i++) {
+  const tipDiscountBtn = document.querySelector(
+    `#tooltip_button_discount_${i}`,
+  );
+  const tipDiscount = document.querySelector(`#tooltip_discount_${i}`);
+
+  toggleTooltip(tipDiscountBtn, tipDiscount);
+}
